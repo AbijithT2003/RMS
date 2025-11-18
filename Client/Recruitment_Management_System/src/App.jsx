50784c5504076becdb8bee6da553bdc5fbcdc36a
AbijithT2003 <jitthu186@gmail.com>
2025-11-18 15:17:49 +0530

    app +dependencies changes

--- Client/Recruitment_Management_System/src/App.jsx
+++ Client/Recruitment_Management_System/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles.css';
 import LandingPage from "./app/layout/LandingPage/LandingPage.jsx";
+import AuthPage from "./pages/auth/AuthPage.jsx";
+import ApplicantDashboard from "./app/dashboard/ApplicantDashboard.jsx";
+import RecruiterDashboard from "./app/dashboard/RecruiterDashboard.jsx";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/applicant-dashboard" element={<ApplicantDashboard />} />
+        <Route path="/recruiter-dashboard" element={<RecruiterDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
